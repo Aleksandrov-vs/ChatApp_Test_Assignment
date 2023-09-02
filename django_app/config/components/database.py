6644b7742d887c36369a4fa1c501a598a2c8eb03
vasya <dfcz200115@mail.ru>
2023-09-02 19:29:34 +0600

    feat: init django app

---
+++ django_app/config/components/database.py
@@ -0,0 +1,16 @@
+import logging
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE'),
+        'USER': os.environ.get('MYSQL_USER'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
+        'HOST': os.environ.get('MYSQL_HOST'),
+        'PORT': os.environ.get('MYSQL_PORT'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
+    }
+}
